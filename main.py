934d8bca6d631eaaa326d7f9cb0994a2cd8fd256
Vlad <vlad.voskanyan@gmail.com>
2025-11-17 23:14:30 -0500

    Add movement to sprite

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-
+import player
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    p = player.Player(x, y)
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        p.update(dt)
+        p.draw(screen)
         pygame.display.flip()
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = (clock.tick(60) / 1000)
+        print(f"{dt}")
 
 
 if __name__ == "__main__":
